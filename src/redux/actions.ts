7b1c6125ac17c0c233ecba22795f2018c63cd6e8
vitalyvitmens <otm_@tut.by>
2024-03-25 19:59:06 +0300

    chore(src\redux\actions.ts): убирает лишние actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -3,9 +3,6 @@ import { ContactDto } from 'src/types/dto/ContactDto'
 export const ADD_NEW_CONTACT_ACTION = 'ADD_NEW_CONTACT'
 export const REMOVE_CONTACT_ACTION = 'REMOVE_CONTACT'
 
-export const ADD_TO_FAVORITES_ACTION = 'ADD_TO_FAVORITES'
-export const REMOVE_FROM_FAVORITES_ACTION = 'REMOVE_FROM_FAVORITES'
-
 interface AddNewContactAction {
   type: typeof ADD_NEW_CONTACT_ACTION
   payload: {
@@ -15,20 +12,6 @@ interface AddNewContactAction {
 
 interface RemoveContactAction {
   type: typeof REMOVE_CONTACT_ACTION
-  payload: {
-    id: ContactDto['id']
-  }
-}
-
-interface AddToFavoritesAction {
-  type: typeof ADD_TO_FAVORITES_ACTION
-  payload: {
-    contact: ContactDto
-  }
-}
-
-interface RemoveFromFavoritesAction {
-  type: typeof REMOVE_FROM_FAVORITES_ACTION
   payload: {
     id: ContactDto['id']
   }
@@ -46,20 +29,4 @@ export function RemoveContactActionCreator(
   return { type: REMOVE_CONTACT_ACTION, payload: { id } }
 }
 
-export function addToFavoritesActionCreator(
-  contact: ContactDto
-): AddToFavoritesAction {
-  return { type: ADD_TO_FAVORITES_ACTION, payload: { contact } }
-}
-
-export function removeFromFavoritesActionCreator(
-  id: ContactDto['id']
-): RemoveFromFavoritesAction {
-  return { type: REMOVE_FROM_FAVORITES_ACTION, payload: { id } }
-}
-
-export type ProjectActions =
-  | AddNewContactAction
-  | RemoveContactAction
-  | AddToFavoritesAction
-  | RemoveFromFavoritesAction
+export type ProjectActions = AddNewContactAction | RemoveContactAction
