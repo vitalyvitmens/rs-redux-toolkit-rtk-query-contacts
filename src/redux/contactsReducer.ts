4010bef92aa1e44dba54e724e069ec5026c58ce8
vitalyvitmens <otm_@tut.by>
2024-03-25 23:03:23 +0300

    refactor: правит код

--- src/redux/contactsReducer.ts
+++ src/redux/contactsReducer.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { ContactDto } from 'src/types/dto/ContactDto'
 import { contacts } from 'src/__data__'
 
-export const createContacts = createAsyncThunk('createContacts', async () => {
+const createContacts = createAsyncThunk('createContacts', async () => {
   const res = await fetch(
     'https://fs.gcfiles.net/fileservice/file/download/a/177331/sc/190/h/560e0501fa0e19aed9ef169df6095f00.json'
   )
@@ -17,4 +17,26 @@ export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: contacts as ContactDto[],
   reducers: {},
+  // extraReducers: (builder) => {
+  //   builder.addMatcher(createContacts.pending.match, () => {
+  //     return {
+  //       loading: true,
+  //       confirmed: false,
+  //     }
+  //   })
+
+  //   builder.addMatcher(createContacts.fulfilled.match, () => {
+  //     return {
+  //       loading: false,
+  //       confirmed: true,
+  //     }
+  //   })
+
+  //   builder.addMatcher(createContacts.rejected.match, () => {
+  //     return {
+  //       loading: false,
+  //       confirmed: false,
+  //     }
+  //   })
+  // },
 })
--- src/redux/groupContactsReducer.ts
+++ src/redux/groupContactsReducer.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
 import { groupContacts } from 'src/__data__'
 
-export const createGroupContacts = createAsyncThunk(
+const createGroupContacts = createAsyncThunk(
   'createGroupContacts',
   async () => {
     const res = await fetch(
@@ -20,4 +20,26 @@ export const groupContactsSlice = createSlice({
   name: 'groupContacts',
   initialState: groupContacts as GroupContactsDto[],
   reducers: {},
+  // extraReducers: (builder) => {
+  //   builder.addMatcher(createGroupContacts.pending.match, () => {
+  //     return {
+  //       loading: true,
+  //       confirmed: false,
+  //     }
+  //   })
+
+  //   builder.addMatcher(createGroupContacts.fulfilled.match, () => {
+  //     return {
+  //       loading: false,
+  //       confirmed: true,
+  //     }
+  //   })
+
+  //   builder.addMatcher(createGroupContacts.rejected.match, () => {
+  //     return {
+  //       loading: false,
+  //       confirmed: false,
+  //     }
+  //   })
+  // },
 })
