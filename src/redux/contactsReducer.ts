81c8f26e4c6dd6cadcc7019721e131ad1c9a5662
vitalyvitmens <otm_@tut.by>
2024-03-25 22:41:07 +0300

    feat: добавляет createAsyncThunk

--- src/redux/contactsReducer.ts
+++ src/redux/contactsReducer.ts
@@ -1,6 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { ContactDto } from 'src/types/dto/ContactDto'
 import { contacts } from 'src/__data__'
+
+export const createContacts = createAsyncThunk('createContacts', async () => {
+  const res = await fetch(
+    'https://fs.gcfiles.net/fileservice/file/download/a/177331/sc/190/h/560e0501fa0e19aed9ef169df6095f00.json'
+  )
+  const data: ContactDto = await res.json()
+
+  if (!data.id) {
+    throw new Error('Something goes wrong')
+  }
+})
 
 export const contactsSlice = createSlice({
   name: 'contacts',
--- src/redux/groupContactsReducer.ts
+++ src/redux/groupContactsReducer.ts
@@ -1,6 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
 import { groupContacts } from 'src/__data__'
+
+export const createGroupContacts = createAsyncThunk(
+  'createGroupContacts',
+  async () => {
+    const res = await fetch(
+      'https://fs.gcfiles.net/fileservice/file/download/a/177331/sc/503/h/03b7cef5194e433422491a8f22413a18.json'
+    )
+    const data: GroupContactsDto = await res.json()
+
+    if (!data.id) {
+      throw new Error('Something goes wrong')
+    }
+  }
+)
 
 export const groupContactsSlice = createSlice({
   name: 'groupContacts',
