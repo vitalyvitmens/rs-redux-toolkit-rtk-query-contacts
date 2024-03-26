bbf0612be14e65807296b2d3732334b046db5b53
vitalyvitmens <otm_@tut.by>
2024-03-26 15:28:55 +0300

    refactor: рефакторит код

--- src/redux/logActionMiddleware.ts
+++ src/redux/logActionMiddleware.ts
@@ -3,7 +3,9 @@ import { RootState } from './store'
 import { logAction } from '../metrics/logAction'
 
 export const logActionMiddleware: Middleware<{}, RootState> =
-  (storeAPI) => (next) => (action) => {
+  (storeAPI) =>
+  (next) =>
+  (action): ReturnType<typeof next> => {
     if (typeof action === 'object' && action !== null && 'type' in action) {
       logAction(action)
     }
