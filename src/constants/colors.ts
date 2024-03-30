ce177bc2782ce57aed0b90e206c8a945b18acfbd
vitalyvitmens <otm_@tut.by>
2024-03-30 17:55:12 +0300

    feat(src\constants\colors.ts): добавляет файл с константами цвета

---
+++ src/constants/colors.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  primary: '#FFC94C',
+  success: '#008000',
+  black: '#000000',
+  white: '#fafafa',
+  yellow: '#FFFF00',
+  green: '#008000',
+  tan: '#FFD97F',
+  brown: '#A52A2A',
+  blue: '#0000FF',
+  olive: '#996F00',
+  red: '#FF0000',
+  darkBrown: '#332400',
+  bootstrapBlue: '#007bff',
+  bisque: '#ffe4b5',
+}
